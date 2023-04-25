ae0cdf3db2432d8301bb8d305123a073f2fa1cb6
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-04-25 15:20:32 -0500

    added level functionality, no need for xp/gold yet

--- src/app/components/level/level.component.ts
+++ src/app/components/level/level.component.ts
@@ -1,22 +1,79 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input,OnInit,OnChanges, Output, EventEmitter } from '@angular/core';
 import { Character } from 'src/app/models/character';
+
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-level',
   templateUrl: './level.component.html',
   styleUrls: ['./level.component.css']
 })
-export class LevelComponent {
+export class LevelComponent implements OnInit{
 @Input() character!: Character;
+@Input() levelBool!: boolean;
 
 plus:string = "+";
 minus:string = "-";
 Attributes!:number[];
-// onChanges() {
+attributeNames: string[] = ["Strength", "Dexterity", "Constitution", "Intelligence"]
+saveChanges:string = "Save Changes";
+back:string = "Back"
+
+
+constructor(private messageService:MessageService) {}
+// OnChanges() {
 //   if (this.character) {
 //     this.Attributes = [this.character.strength, this.character.dexterity, this.character.constitution, this.character.intelligence]
 //     console.warn("stats")
 //     console.warn(this.Attributes)
 //   }
 // }
+incrementStat(i:number) {
+  this.Attributes[i] += 1;
+}
+
+decrementStat(i:number): void {
+  switch (i) {
+    case 0 : {
+        if (this.Attributes[i] > this.character.strength) {
+          this.Attributes[i] -= 1;
+        }
+        break;
+      }
+    case 1 : {
+        if (this.Attributes[i] > this.character.dexterity) {
+          this.Attributes[i] -= 1;
+        }
+        break;
+      }
+    case 2: {
+        if (this.Attributes[i] > this.character.constitution) {
+          this.Attributes[i] -= 1;
+        }
+        break;
+      }
+    case 3 : {
+        if (this.Attributes[i] > this.character.intelligence) {
+          this.Attributes[i] -= 1;
+        }
+        break;
+      }
+  }
+}
+
+saveStats(): void {
+  this.character.strength = this.Attributes[0];
+  this.character.dexterity = this.Attributes[1];
+  this.character.constitution = this.Attributes[2];
+  this.character.intelligence = this.Attributes[3];
+}
+
+@Output() ReturnToMainInn = new EventEmitter<boolean>();
+returnToInn(): void {
+  this.ReturnToMainInn.emit(true);
+}
+
+ngOnInit(): void {
+this.Attributes = [this.character.strength, this.character.dexterity, this.character.constitution, this.character.intelligence]
+}
 }
