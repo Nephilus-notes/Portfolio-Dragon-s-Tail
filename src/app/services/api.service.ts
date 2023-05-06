9821dfbde04fac723c906a07d0cfe3ff7674af03
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-05-06 12:33:18 -0500

    docs to api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -23,17 +23,16 @@ export class ApiService {
   npcCache!: Character;
   locationCache!:Location;
   locations!: Location[];
-
+/**
+ * Stored values for building a character during the new game process
+ */
   templateCache!: Array<Template>;
   /** 
   * Asynchronous function to perform an api call to retrieve the character based on its ID
-  *
-  * @returns A character object 
-  *
-  *
-  *
+  * @param charID a number associated with a character in the database
+  * @returns An observable of a  full character object ready to be loaded
   */
-  getCharacter(charID: number): Observable<Character> {
+  public getCharacter(charID: number): Observable<Character> {
   
     let url = `${environment.characterURL}${charID}`
     const character = this.http.get<Character>(url)
@@ -44,37 +43,33 @@ export class ApiService {
 
   /** 
   * Caches the retrieved character into state so it can be accessed by other components 
-  * without extraneous API calls
+  * and views without extraneous API calls
   *
   * @param character - Character type object to be cached
   * @param charID - temp param until character has an id field on it.
   * @returns none
   *
   */
-  cacheCharacter(character:Character) {
+  public cacheCharacter(character:Character) {
     // console.warn(`character cache: ${character}`)
     this.characterCache = character
-    this.messageService.add('Progress Saved')
+    this.messageService.add('character cached')
   }
   
   /** 
   * Loads a character from the CharacterService cache without an API call
-  *
   * @param none
-  * @returns none
-  *
-  *
+  * @returns A character object
   */
-
-
-  // public clearCharacterCache(): void {
-  //   delete this.characterCache
-  // }
   public loadCharacter(): Character {
     this.messageService.add('Character Loaded from characterservice')
     return this.characterCache
   }
 
+  /**
+   * Makes an api call to save the character's changes into the database
+   * @param character The current player character
+   */
   public patchCharacter(character:Character): void {
     let url = `${environment.characterURL}${character.id}`
     let charDTO: characterDTO = {
@@ -87,13 +82,14 @@ export class ApiService {
     this.messageService.add("success, but how do we measure it?")
   }
 
-
+/**
+   * API call to the database to save a new character
+   * @param character 
+   * @returns An observable of the newly saved character object.  
+   */
   public postCharacter(character:Character): Observable<Character> {
     let url = `${environment.characterURL}`
     let date = new Date().toISOString()
-
-
-    // console.warn(date)
 
     let charDTO: characterDTO = {
       dateAdded:date,
@@ -116,7 +112,7 @@ export class ApiService {
    * 
    * @returns - Boolean
    */
-  characterCacheExists() {
+  public characterCacheExists() {
     if (this.characterCache) {
       this.messageService.add('Character exists!')
       return true
@@ -130,24 +126,24 @@ export class ApiService {
 
  
     /** 
-    * Asynchronous function to perform an api call to retrieve the character based on its ID
+    * Asynchronous function to perform an api call to retrieve an NPC's base values using  its ID
     *
-    * @returns A character object 
-    *
-    *
-    *
+    * @returns An observable NPC object 
     */
-    getEnemy(NPCID:number): Observable<NPC> {
+    public getEnemy(NPCID:number): Observable<NPC> {
 
       let url = `${environment.NpcURL}${NPCID}`
     const enemy = this.http.get<NPC>(url)
 
     this.messageService.add('CharacterService: fetched enemy')
     return enemy;
-
     }
 
-    getTemplates(): Observable<Array<Template>>{
+    /**
+     * Retrieves all character default values for players to choose their start
+     * @returns An observable that contains an array of character default objects
+     */
+    public getTemplates(): Observable<Array<Template>>{
       let url = `${environment.templateURL}`
       const templateList = this.http.get<Array<Template>>(url)
   
@@ -155,15 +151,27 @@ export class ApiService {
       return templateList;
     }
 
-    cacheTemplates(templateList:Array<Template>): void {
+    /**
+     * Takes a list of character defaults and caches them to persist across component changes
+     * @param templateList 
+     */
+    public cacheTemplates(templateList:Array<Template>): void {
       this.templateCache = templateList;
     };
 
-    loadTemplates(): Array<Template> {
+     /**
+     * 
+     * @returns An array of all default character objects 
+     */
+    public loadTemplates(): Array<Template> {
       return this.templateCache;
     };
 
-    templateCacheExists() {
+     /**
+     * Checks to see if a templateCache has been instantiated
+     * 
+     */
+    public templateCacheExists(): boolean {
       if (this.templateCache) {
         this.messageService.add('Templates exists!')
         return true
@@ -174,7 +182,12 @@ export class ApiService {
       }
     };
 
-    getItem(itemID: number): Observable<Item> {
+    /**
+     * API call to retrieve an item object from the database
+     * @param itemID the item's primary key
+     * @returns an observable of an item from the database
+     */
+    public getItem(itemID: number): Observable<Item> {
   
       let url = `${environment.itemURL}${itemID}`
       const item = this.http.get<Item>(url)
@@ -182,7 +195,13 @@ export class ApiService {
       return item;
     }
   
-    getItems(idList:Array<number>): Array<Observable<Item>> {
+    
+    /**
+     * Makes multiple API calls for items based on primary keys in the idList
+     * @param idList a list of primary keys
+     * @returns an array of observables
+     */
+    public getItems(idList:Array<number>): Array<Observable<Item>> {
       let items = new Array<Observable<Item>>
   
       for(let i = 0; i < idList.length; i ++) {
@@ -200,14 +219,14 @@ export class ApiService {
    * sanitized user input
    * @returns a location object
    */
-  getNewLocation(loc_id:string): Observable<Location> {
+  public getNewLocation(loc_id:string): Observable<Location> {
     let url = `${environment.locationURL}${loc_id}`;
 
     const location = this.http.get<Location>(url)
     return location
   }
 
-  saveLocation(location:Location): void {
+  public saveLocation(location:Location): void {
     this.locationCache = location
     // console.warn(location)
     this.messageService.add('location Saved')
@@ -220,31 +239,43 @@ export class ApiService {
   *
   *
   */
-  loadLocation(): Location {
+  public loadLocation(): Location {
     this.messageService.add('location Loaded')
     return this.locationCache
   }
 
-  cacheExists(): boolean {
+  /**
+   * Checks if a location has been stored in state
+   * @returns a boolean 
+   */
+  public cacheExists(): boolean {
     if(this.locationCache) {
       return true
     }
     return false
   }
 
-  getLocations() {
+  /**
+   * Gets a list of all locations in the database
+   * @returns an Observable of an arary of location objects
+   */
+  public getLocations(): Observable<Array<Location>> {
 
     let url = environment.locationURL;
 
-    return this.http.get<any>(url)
+    return this.http.get<Array<Location>>(url)
   }
 
-  getCharacterTemplate(TemplateID: String): Observable<Template> {
+  /**
+   * API call to retrieve a single character default template from the database
+   * @returns An observable of a character default template
+   */
+  public getCharacterTemplate(TemplateID: String): Observable<Template> {
   
     let url = `${environment.templateURL}${TemplateID}`
-    const character = this.http.get<Template>(url)
+    const template = this.http.get<Template>(url)
 
-    this.messageService.add('CharacterService: fetched characters')
-    return character;
+    this.messageService.add('API: got single template')
+    return template;
   };
   }
