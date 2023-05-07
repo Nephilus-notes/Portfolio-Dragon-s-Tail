566e94fd254a9516d83b0ab3e372f48052cfa66a
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-05-07 17:14:30 -0500

    added savefile docs

--- src/app/models/saveFile.ts
+++ src/app/models/saveFile.ts
@@ -1,5 +1,6 @@
 /**
- * 
+ * A database object used for retrieving saved games.  Keys: id, userID, 
+ * playerCharacterID, locationID, dateUpdated, dateAdded, characterName
  */
 
 export interface SaveFile {
