39f3ccc3be29e0b4bfb3ef94f23708de5dcad614
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-05-08 23:40:02 -0500

    added docs to combat service

--- src/app/services/combat-controller.service.ts
+++ src/app/services/combat-controller.service.ts
@@ -13,6 +13,15 @@ export class CombatControllerService {
   playerCharacter!: Character;
   NPCEnemy!: NPC;
 
+  /**
+   * Generates a random number to determine if a critical hit happened,
+   * then checks against the targets evasion to see if they are hit,
+   * finally calculates damage if they are hit and calls the dealDamage function.
+   * @param self The player or non player character doing the attacking
+   * @param target The target of the violence
+   * @param type A string with a default value of physical, but a value of magical would change 
+   * the stats tested
+   */
   public attack(self:Character|NPC, target: Character|NPC, type:string="physical"): void {
     let attack: number = Math.random() * 100;
 
@@ -34,22 +43,41 @@ export class CombatControllerService {
     } 
   }
 
+  /**
+   * Deals damage to the char equal to the damage passed in.
+   * @param char The player or non player character that is recieving damage
+   * @param damage The amount of damage recieved
+   */
   public dealDamage(char:Character|NPC, damage: number): void {
     if (damage > 0) {
       char.currentHP -= damage
     }
   }
 
+  /**
+   * LEGACY an ability the player can use to briefly boost their armor
+   * @param self the character performing the action
+   */
   public defend(self: Character): void {
     self.armorValue += 2
     this.messageService.add(`${self.name} focuses on defense`, true)
   }
 
+  /**
+   * LEGACY an ability the player can use to briefly boost their evasion
+   * @param self the character performing the action
+   */
   public evade(self: Character): void {
     self.evadePercentage += 2
     this.messageService.add(`${self.name} focuses on evasion`, true)
   }
 
+  /**
+   * an ability used to try to escape combat.
+   * @param self the player or non player character using the ability
+   * @param target The enemy whose speed is being compared
+   * @returns A boolean. true = self is faster and will leave combat. false = the enemy is faster and self cannot escape
+   */
   public flee(self: Character|NPC, target: Character|NPC): boolean {
     if (self.dexterity > target.dexterity) {
       this.messageService.add(`${self.name} has fled!`, true)
@@ -60,7 +88,11 @@ export class CombatControllerService {
       return false
     }
   }
-
+  /**
+   * Heals the character by the amount passed in.
+   * @param self the pc or npc to be healed
+   * @param amountHealed The amount of hp regained
+   */
   public heal(self:Character|NPC, amountHealed:number): void {
     self.currentHP += amountHealed;
     if (self.currentHP > self.maxHP) {
@@ -68,7 +100,12 @@ export class CombatControllerService {
     }
   }
 
-  
+  /**
+   * Used at the end of a round, it ensures both combatants are able to continue fighting, and if not returns false and ends combat.
+   * @param player 
+   * @param enemy 
+   * @returns 
+   */
   public checkCombatants(player: Character, enemy:NPC): boolean {
     if (player.fleeing == true) {
       return false
@@ -105,6 +142,13 @@ export class CombatControllerService {
       }
     }
   }
+  /**
+   * A combat round. Uses the two combatants' dexterity to determine order then makes the enemy attack the player and 
+   * performs whatever action dictated by the actionCall string the player submitted
+   * @param character 
+   * @param enemy 
+   * @param actionCall 
+   */
   public round(character: Character, enemy: NPC, actionCall: string) {
 
     if (character.dexterity >= enemy.dexterity) {
@@ -121,6 +165,12 @@ export class CombatControllerService {
 
   }
 
+  /**
+   * A switch that determines which legacy ability the player uses. [Attack, Defend, Evade, Flee]
+   * @param character 
+   * @param enemy 
+   * @param actionCall 
+   */
   public playerAction(character: Character, enemy: NPC, actionCall: string) {
     // this.messageService.add(`player action call ${actionCall}`);
 
@@ -144,17 +194,26 @@ export class CombatControllerService {
     }
   }
 
+  /**
+   * Takes the essence from the slain enemy and adds it to the players currentCurrency
+   * @param character 
+   * @param enemy 
+   */
   private gainLoot(character: Character, enemy: NPC): void {
     character.currentCurrency += enemy.currentCurrency;
     character.lifeTimeCurrency += enemy.currentCurrency;
   }
 
-  // startCombat(player: Character, enemy:NPC) {
-  //   this.setTempAttributes(player)
-  //   this.setTempAttributes(enemy)
-  // };
-
-
+/**
+ * New function designed to take in an ability object and us it to perform an action
+ * @param self The PC or NPC using the ability
+ * @param target The target of the abiity
+ * @param effect A string: options "damage", "heal", "buff", and "debuff"?
+ * @param affectedAttribute A string that maps to a particular attribute on the character object
+ * @param modifier A number that increases the effect of the ability
+ * @param duration A number indicating the number of rounds the effect persists
+ * @param type A string: "physical" and "magical" indicating which attribute should be used for damage (strength vs intelligence) against which defense (armor vs resistance)
+ */
   public performAbility(self: Character| NPC, target: Character| NPC, effect:string, affectedAttribute:string, modifier:number=1, duration:number=0, type:string="physical" ) {
     if (effect == "damage") {
       if (type == "physical") {
